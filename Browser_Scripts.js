93009e04e463a2d23f55e2221a023bdc5a4e21bf
Kenai River Rhodes <kenai.rhodes@gmail.com>
2021-10-10 11:22:43 -0400

    Update Browser_Scripts.js

--- Browser_Scripts.js
+++ Browser_Scripts.js
@@ -1,4 +1,20 @@
 // Follow all of an account's followers
+
+// The wake lock sentinel.
+let wakeLock = null;
+
+// Function that attempts to request a wake lock.
+const requestWakeLock = async () => {
+  try {
+    wakeLock = await navigator.wakeLock.request('screen');
+    wakeLock.addEventListener('release', () => {
+      console.log('Wake Lock was released');
+    });
+    console.log('Wake Lock is active');
+  } catch (err) {
+    console.error(`${err.name}, ${err.message}`);
+  }
+};
 setInterval(
     function(){
         var arr = document.getElementsByClassName("sqdOP  L3NKy   y3zKF     ");
